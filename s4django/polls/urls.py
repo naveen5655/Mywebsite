c176775ed64943320d8efef71c5226e5a63b8bdc
Naveen <2000031509@kluniversity.in>
2022-03-15 11:09:57 +0530

    review-2

--- s4django/polls/urls.py
+++ s4django/polls/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns = [
   path('languages/',views.languages,name='languages'),
   path('projects/',views.projects,name='projects'),
--- s4django/polls/views.py
+++ s4django/polls/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 def index(request):
     context = {}
     return render(request,'polls/index.html',context)
